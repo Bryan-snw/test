ad756233795760d72eb2be3b163baaaa00a78c5d
BRYAN <bryansie29@gmail.com>
2020-12-10 09:47:56 +0800

    coba coba

---
+++ src/percobaan/Percobaan.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Percobaan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        int a = 10;
+        int b =10;
+        
+        System.out.println("Post Increment");
+        System.out.println("Nilai a    :"+ a);
+        System.out.println("Nilai a ++ :"+ a++);
+        System.out.println("Nilai a    :"+ a);
+        
+        System.out.println();
+        System.out.println("Pre Increment");
+        System.out.println("Nilai b    :"+ b);
+        System.out.println("Nilai ++ b :"+ ++b);
+        System.out.println("Nilai b    :"+ b);
+        */
+        Perkalian m = new Perkalian();
+        m.perk(5, 5);
+    }
+    
+}
---
+++ src/percobaan/Perkalian.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan;
+
+
+public class Perkalian {
+    int a = 5;
+    int b = 5;
+    
+    public void perk(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println(a*b);
+    }
+}
